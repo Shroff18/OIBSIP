16c02a0c40b27883366ef9493142f2f35171c42a
Shroff18 <131897187+Shroff18@users.noreply.github.com>
2023-08-31 15:50:27 +0530

    Add files via upload

---
+++ LEVEL2_TASK4/main.js
@@ -0,0 +1,10 @@
+function call() {
+    const ipt = document.getElementById('inputtxt').value
+    const psw = document.getElementById('password').value
+    if (ipt == "" || psw == "") {
+        // alert("fill the form ")
+    }
+    else {
+        alert("succesfully submit")
+    }
+}
